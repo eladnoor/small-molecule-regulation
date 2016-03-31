1aff315131f989dc4334a7478a4bbbe9aa06d435
Elad Noor <elad.noor@gmail.com>
2016-03-31 19:16:01 +0200

    calculating spearman correlations for K_I and K_M vs concentrations

--- python/KMandKI_vs_conc.py
+++ python/KMandKI_vs_conc.py
@@ -7,6 +7,8 @@ Created on Thu Mar 31 11:03:03 2016
 
 import os
 import pandas as pd
+import scipy
+import numpy as np
 import settings
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -28,8 +30,8 @@ ki = ki[ki['K_I'] != -999]
 km_median = km.groupby('bigg.metabolite').median()
 ki_median = ki.groupby('bigg.metabolite').median()
 
-data = km_median.join(ki_median, how='outer').join(met_conc_mean.iloc[:, [0, 3]], how='outer')
-#data = np.log10(data)
+data = km_median.join(ki_median, how='outer')
+data = data.join(met_conc_mean.iloc[:, [0, 3]], how='outer')
 
 concensus = data[~data.isnull().any(axis=1)]
 
@@ -52,7 +54,40 @@ for ax, carbon_source in zip(axs, ['Glucose', 'Acetate']):
     
     ax.legend(loc='upper left')
     ax.set_xlabel('mean concentration on %s [mM]' % carbon_source)
-    ax.set_ylabel('mean concentration on %s [mM]' % carbon_source)
+    ax.set_ylabel('Affinity [mM]')
     settings.plotdiag(ax=ax, lw=0.5)
 
 fig.savefig(os.path.join(settings.RESULT_DIR, 'KMandKI_vs_conc.svg'))
+
+#%%
+# scatter plot comparing the K_M or K_I and abundance of each metabolite in glucose and acetate
+for param in ['K_M', 'K_I']:
+    fig, axs = plt.subplots(1, 2, figsize=(14, 7))
+    for ax, carbon_source in zip(axs, ['Glucose', 'Acetate']):
+        ax.set_xscale('log')
+        ax.set_yscale('log')
+        x = '%s (mean)' % carbon_source
+        concensus = data[~ (data[x].isnull() | data[param].isnull())]
+        xdata = concensus[x]
+        ydata = concensus[param]
+        for ind in concensus.index:
+            ax.annotate(ind, xy=(xdata[ind], ydata[ind]),
+                        xycoords='data', 
+                        xytext=(0, 5), textcoords='offset points',
+                        ha='center')
+        concensus.plot(kind='scatter', x=x, y=param, ax=ax)
+        
+        ax.legend(loc='upper left')
+        ax.set_xlabel('mean concentration on %s [mM]' % carbon_source)
+        ax.set_ylabel('$%s$ in [mM]' % param)
+        settings.plotdiag(ax=ax, lw=0.5)
+        r = scipy.stats.spearmanr(xdata, ydata)
+        ax.annotate('$r_{spearman}$ = %.2f (p < %.3f)' % (r.correlation, r.pvalue),
+                    xy=(0.04, 0.9), xycoords='axes fraction',
+                    ha='left', va='top', size=15)
+    
+    fig.savefig(os.path.join(settings.RESULT_DIR, '%s_vs_conc.svg' % param))
+    #
+
+
+
