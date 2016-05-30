85aa8b8cbff259e923f65de4aa95b86f53e320a4
Elad Noor <elad.noor@gmail.com>
2016-05-30 15:54:28 +0200

    Added a heatmap of the [S]/Km and [S]/Ki values

--- python/KMandKI_vs_conc.py
+++ python/KMandKI_vs_conc.py
@@ -12,10 +12,13 @@ import numpy as np
 import settings
 import matplotlib.pyplot as plt
 import seaborn as sns
-sns.set_style('ticks')
+#sns.set_style('ticks')
+sns.axes_style('whitegrid')
 
-_df = pd.DataFrame.from_csv(os.path.join(settings.DATA_DIR, 'ecoli_metabolites_gerosa2015.csv'),
+_df = pd.DataFrame.from_csv(os.path.join(settings.DATA_DIR,
+                                         'ecoli_metabolites_gerosa2015.csv'),
                             index_col=0)
+_df.index.name = 'bigg.metabolite'
 met_conc_mean = _df.iloc[:, 1:9]
 met_conc_std = _df.iloc[:, 10:]
 
@@ -27,11 +30,11 @@ ki.rename(columns={'Value':'K_I'}, inplace=True)
 km = km[km['K_M'] != -999]
 ki = ki[ki['K_I'] != -999]
 
-km_median = km.groupby('bigg.metabolite').median()
-ki_median = ki.groupby('bigg.metabolite').median()
+km_median = km.groupby('bigg.metabolite')['K_M'].median().reset_index()
+ki_median = ki.groupby('bigg.metabolite')['K_I'].median().reset_index()
 
-data = km_median.join(ki_median, how='outer')
-data = data.join(met_conc_mean.iloc[:, [0, 3]], how='outer')
+data = pd.merge(km_median, ki_median).join(met_conc_mean, on='bigg.metabolite')
+data.set_index('bigg.metabolite', inplace=True)
 
 concensus = data[~data.isnull().any(axis=1)]
 
@@ -89,5 +92,21 @@ for param in ['K_M', 'K_I']:
     fig.savefig(os.path.join(settings.RESULT_DIR, '%s_vs_conc.svg' % param))
     #
 
+#%%
+# draw heat maps of the [S]/Ki and [S]/Km values across the 8 conditions
+ki_fc = np.log2(concensus.iloc[:, 2:].multiply(1.0/concensus['K_I'], axis='index'))
+km_fc = np.log2(concensus.iloc[:, 2:].multiply(1.0/concensus['K_M'], axis='index'))
 
+colmap = dict(map(lambda x: (x, x[:-7]), ki_fc.columns))
+indmap = dict(map(lambda x: (x, x[:-2]), ki_fc.index))
 
+ki_fc.rename(index=indmap, columns=colmap, inplace=True)
+km_fc.rename(index=indmap, columns=colmap, inplace=True)
+fig, (ax0, ax1) = plt.subplots(1, 2, sharey=True, figsize=(20, 10))
+sns.heatmap(km_fc, ax=ax0, mask=km_fc.isnull())
+sns.heatmap(ki_fc, ax=ax1, mask=km_fc.isnull())
+ax0.set_title('$\log_2([S]/K_M)$')
+ax0.set_ylabel('')
+ax1.set_title('$\log_2([S]/K_I)$')
+ax1.set_ylabel('')
+fig.savefig(os.path.join(settings.RESULT_DIR, 'saturation_heatmaps.svg'))
