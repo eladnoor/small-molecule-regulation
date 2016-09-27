41236fe890f0fc6e4db079d72b2c94176d80ea24
Elad Noor <elad.noor@gmail.com>
2016-09-27 23:53:18 +0200

    finilizing merge of all data for volume minimization

--- python/volume_minimization.py
+++ python/volume_minimization.py
@@ -10,82 +10,117 @@ from cobra.io.sbml import create_cobra_model_from_sbml_file
 from cobra.manipulation.modify import convert_to_irreversible
 from bool_parser import BoolParser
 
+def get_metabolite_data():
+    """
+        get the metabolomics data from Gerosa et al. 2015
+    """
+    _df = pd.DataFrame.from_csv(S.ECOLI_METAB_FNAME)
+    _df.index.name = 'bigg.metabolite'
+    met_conc_mean = _df.iloc[:, 1:9] * 1e-3 # convert mM to M
+    met_conc_std = _df.iloc[:, 10:]  * 1e-3 # convert mM to M
+    
+    met_conc_mean.columns = [c[:-7].lower() for c in met_conc_mean.columns]
+    met_conc_std.columns = [c[:-6].lower() for c in met_conc_std.columns]
+    
+    return met_conc_mean
 
-# load the metabolomics data from Gerosa et al. 2015
-_df = pd.DataFrame.from_csv(S.ECOLI_METAB_FNAME)
-_df.index.name = 'bigg.metabolite'
-met_conc_mean = _df.iloc[:, 1:9]
-met_conc_std = _df.iloc[:, 10:]
+def get_km_data():
+    km_df = S.read_cache('km')
+    km_df = km_df[km_df['Organism'] == 'Escherichia coli']
+    km_df = km_df[km_df['KM_Value'] != -999]
+    km_df = km_df[['EC_number', 'KM_Value', 'bigg.metabolite']]
+    
+    # join with ec2bigg table to map values to bigg.reactions
+    ec2bigg = pd.DataFrame.from_csv(S.BIGG2EC_FNAME)
+    km_df = pd.merge(km_df, ec2bigg, on='EC_number')
+    return km_df
 
-met_conc_mean.columns = [c[:-7].lower() for c in met_conc_mean.columns]
-met_conc_std.columns = [c[:-6].lower() for c in met_conc_std.columns]
+def get_enzyme_data():
+    """
+        get the enzyme data from Schmidt et al. 2015
+    """
+    _df = pd.DataFrame.from_csv(S.ECOLI_PROT_FNAME)
+    _df = _df[~pd.isnull(_df['Bnumber'])]
+    _df.set_index('Bnumber', inplace=True)
+    enz_mw         = _df['Molecular weight (Da)']
+    enz_count_mean = _df.iloc[:, 7:29] # counts per 10^-15 liter
+    enz_count_cv   = _df.iloc[:, 30:52]
+    enz_count_mean.columns = [c[:-7].lower() for c in enz_count_mean.columns]
+    enz_count_cv.columns = [c[:-5].lower() for c in enz_count_cv.columns]
+    
+    enz_count_mean = enz_count_mean.apply(pd.to_numeric).fillna(0.0)
+    
+    # for each reaction, get a single value for the enzyme concentration
+    # in [M] and the molecular weight of the complex
+    
+    model = create_cobra_model_from_sbml_file(S.ECOLI_SBML_FNAME)
+    convert_to_irreversible(model)
+    
+    reaction2boolparser = {}
+    for r in model.reactions:
+        if r.gene_reaction_rule == '':
+            continue
+        if r.gene_reaction_rule.find('s0001') != -1:
+            continue
+        reaction2boolparser[r.id.lower()] = BoolParser(r.gene_reaction_rule)
+    
+    enz_conc_df = pd.DataFrame(index=reaction2boolparser.keys(),
+                               columns=enz_count_mean.columns)
+    enz_mass_df = pd.DataFrame(index=reaction2boolparser.keys(),
+                               columns=enz_count_mean.columns)
+    enz_conc_df.index.name = 'bigg.reaction'
+    enz_mass_df.index.name = 'bigg.reaction'
+
+    
+    rho = 1100 # average cell density gr/L
+    DW_fraction = 0.3 # fraction of DW of cells
+    enz_molar = enz_count_mean * 1e15 / 6.02e23 / rho # convert counts/fL to M (intercellular)
+    
+    for c in enz_count_mean.columns:
+        enz_gr_per_gCDW = enz_molar[c] * enz_mw / DW_fraction # convert M to gr/gCDW
+        enzyme_conc_dict = enz_molar[c].to_dict()
+        enzyme_mass_dict = enz_gr_per_gCDW.to_dict()
+        
+        for r, bool_parser in reaction2boolparser.iteritems():
+            enz_conc_df.loc[r, c] = bool_parser.evaluate(enzyme_conc_dict)
+            enz_mass_df.loc[r, c] = bool_parser.evaluate(enzyme_mass_dict)
+    
+    return enz_conc_df, enz_mass_df
 
 
-# load the proteomic data from Schmidt et al. 2015
-_df = pd.DataFrame.from_csv(S.ECOLI_PROT_FNAME)
-_df = _df[~pd.isnull(_df['Bnumber'])]
-_df.set_index('Bnumber', inplace=True)
-enz_mw         = _df['Molecular weight (Da)']
-enz_count_mean = _df.iloc[:, 7:29] # counts per 10^-15 liter
-enz_count_cv   = _df.iloc[:, 30:52]
-enz_count_mean.columns = [c[:-7].lower() for c in enz_count_mean.columns]
-enz_count_cv.columns = [c[:-5].lower() for c in enz_count_cv.columns]
-
-enz_count_mean = enz_count_mean.apply(pd.to_numeric).fillna(0.0)
-
-# merge with Km data
-km = S.read_cache('km')
-km = km[km['Organism'] == 'Escherichia coli']
-km = km[km['KM_Value'] != -999]
-
-met_data = km[['EC_number', 'KM_Value', 'bigg.metabolite']].join(met_conc_mean, on='bigg.metabolite', how='inner')
-
-# map the EC numbers to bigg.reaction, note that there might be duplicates
-# since this is many-to-many 
-bigg2ec_df = pd.DataFrame.from_csv(S.BIGG2EC_FNAME)
-met_data = pd.merge(met_data, bigg2ec_df, on='EC_number')
+##############################################################################
+met_conc_df = get_metabolite_data()
+km_df = get_km_data()
+enz_conc_df, enz_mass_df = get_enzyme_data()
 
 # get the flux from FBA
 flux_df = pd.DataFrame.from_csv(S.ECOLI_FLUX_FNAME)
 flux_df.index.name = 'bigg.reaction'
+flux_df.reset_index(inplace=True)
+flux_df['bigg.reaction'] = flux_df['bigg.reaction'].apply(str.lower)
 
-# keep only the intersection of all conditions 
+#%%
+enz_conc_df = pd.melt(enz_conc_df.reset_index(), id_vars='bigg.reaction',
+                      var_name='condition', value_name='enzyme conc [M]')
+enz_mass_df = pd.melt(enz_mass_df.reset_index(), id_vars='bigg.reaction',
+                      var_name='condition', value_name='enzyme mass [gr/gCDW]')
+met_conc_df = pd.melt(met_conc_df.reset_index(), id_vars='bigg.metabolite',
+                      var_name='condition', value_name='metabolite conc [M]')
+flux_df = pd.melt(flux_df, id_vars='bigg.reaction',
+                      var_name='condition', value_name='flux [mmol/gCDW/h]')
+
+#%%
+data_df = pd.merge(km_df, met_conc_df, on='bigg.metabolite')
+data_df = pd.merge(data_df, enz_conc_df, on=['bigg.reaction', 'condition'])
+data_df = pd.merge(data_df, enz_mass_df, on=['bigg.reaction', 'condition'])
+data_df = pd.merge(data_df, flux_df, on=['bigg.reaction', 'condition'])
+
+# keep only rows with non-zero flux
+data_df = data_df[data_df['flux [mmol/gCDW/h]'] != 0]
+# keep only rows with non-zero enzyme concentration
+data_df = data_df[data_df['enzyme conc [M]'] > 0]
+
+
+# keep only the intersection of all conditions
 # for which we have metabolites, proteins and flux
-cond = met_conc_mean.columns & enz_count_mean.columns & flux_df.columns
-met_conc_mean = met_conc_mean[cond]
-met_conc_std = met_conc_std[cond]
-enz_count_mean = enz_count_mean[cond]
-enz_count_cv = enz_count_cv[cond]
-flux_df = flux_df[cond]
-
-#%% for each reaction, get a single value for the enzyme concentration
-#   in [M] and the molecular weight of the complex
-
-model = create_cobra_model_from_sbml_file(S.ECOLI_SBML_FNAME)
-convert_to_irreversible(model)
-
-gene_df = pd.DataFrame(data=[(r.id, r.gene_reaction_rule) for r in model.reactions],
-                       columns=('bigg.reaction', 'gene_reaction_rule'))
-gene_df.set_index('bigg.reaction', inplace=True)
-
-enz_data = pd.DataFrame(index=gene_df.index, columns=cond)
-
-for c in cond:
-    gene_df[c] = 0
-    
-    rho = 1100 # average cell density gr/L
-    DW_fraction = 0.3 # fraction of DW of cells
-    enz_molar       = enz_count_mean[c] * 1e15 / 6.02e23 # convert counts/fL to moles/L (i.e. M)
-    enz_gr_per_gCDW = enz_molar * enz_mw / (rho * DW_fraction) # convert M to gr/gCDW
-    enzyme_mass_dict = enz_gr_per_gCDW.to_dict()
-    
-    for r in gene_df.index:
-        if gene_df.loc[r, 'gene_reaction_rule'] == '':
-            continue
-        if gene_df.loc[r, 'gene_reaction_rule'].find('s0001') != -1:
-            continue
-        
-        bool_parser = BoolParser(gene_df.loc[r, 'gene_reaction_rule'])
-        gene_df.loc[r, c] = bool_parser.evaluate(enzyme_mass_dict)
-        enz_data.loc[r, c] = bool_parser.evaluate(enzyme_mass_dict)
-#%% 
+#cond = met_conc_df.columns & enz_conc_df.columns & flux_df.columns
