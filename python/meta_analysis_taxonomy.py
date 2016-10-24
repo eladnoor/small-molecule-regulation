2d1bc1d19ebe0ba44d1bf347e7161d79f6d6f675
Ed <reznik.ed@gmail.com>
2016-10-24 15:57:05 -0400

    updated cross taxa figure

--- python/meta_analysis_taxonomy.py
+++ python/meta_analysis_taxonomy.py
@@ -39,7 +39,7 @@ ccm.index = ccm['EC']
 
 ki = S.read_cache('inhibiting')
 act = S.read_cache('activating')
-tax = S.read_cache('TaxonomicData')
+tax = S.read_cache('TaxonomicData_temp')
 
 # Drop entries without organism
 ki = ki[pd.notnull(ki['Organism'])]
